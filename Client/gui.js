023bc608f1012afac03b939251ab8b268feceb1d
Alex Dollar <ADOLLAR@stcharlessd.org>
2020-12-07 07:08:20 -0600

    Updated GUI

--- Client/gui.js
+++ Client/gui.js
@@ -1,18 +1,24 @@
 class GUI {
     constructor(_playerdata) {
         this.playerdata = _playerdata; //PlayerData
-        this.cowUp = createElement("button", "↑");
-        this.woodUp = createElement("button", "↑");
-        this.oreUp = createElement("button", "↑");
-        this.fishUp = createElement("button", "↑");
-        this.clayUp = createElement("button", "↑");
-        this.glassUp = createElement("button", "↑");
-        this.cowDown = createElement("button", "↓");
-        this.woodDown = createElement("button", "↓");
-        this.oreDown = createElement("button", "↓");
-        this.fishDown = createElement("button", "↓");
-        this.clayDown = createElement("button", "↓");
-        this.glassDown = createElement("button", "↓");
+        this.cowUp = createElement("button", "<i class='fas fa-chevron-up'></i>");
+        this.woodUp = createElement("button", "<i class='fas fa-chevron-up'></i>");
+        this.oreUp = createElement("button", "<i class='fas fa-chevron-up'></i>");
+        this.fishUp = createElement("button", "<i class='fas fa-chevron-up'></i>");
+        this.clayUp = createElement("button", "<i class='fas fa-chevron-up'></i>");
+        this.glassUp = createElement("button", "<i class='fas fa-chevron-up'></i>");
+        this.cowDown = createElement("button", "<i class='fas fa-chevron-down'></i>");
+        this.woodDown = createElement("button", "<i class='fas fa-chevron-down'></i>");
+        this.oreDown = createElement("button", "<i class='fas fa-chevron-down'></i>");
+        this.fishDown = createElement("button", "<i class='fas fa-chevron-down'></i>");
+        this.clayDown = createElement("button", "<i class='fas fa-chevron-down'></i>");
+        this.glassDown = createElement("button", "<i class='fas fa-chevron-down'></i>");
+        this.cowLabel = createElement("p", "0");
+        this.woodLabel = createElement("p", "0");
+        this.oreLabel = createElement("p", "0");
+        this.fishLabel = createElement("p", "0");
+        this.clayLabel = createElement("p", "0");
+        this.glassLabel = createElement("p", "0");
 
         this.cowUp.parent("cowUp");
         this.woodUp.parent("woodUp");
@@ -26,5 +32,11 @@ class GUI {
         this.fishDown.parent("fishDown");
         this.clayDown.parent("clayDown");
         this.glassDown.parent("glassDown");
+        this.cowLabel.parent("cowLabel");
+        this.woodLabel.parent("woodlabel");
+        this.oreLabel.parent("oreLabel");
+        this.fishLabel.parent("fishLabel");
+        this.clayLabel.parent("clayLabel");
+        this.glassLabel.parent("glassLabel");
     }
 }
